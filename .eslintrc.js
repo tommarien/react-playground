9b5cccd20ecf3aae8360b9372e1a767f372338e0
Tom Marien <tommarien@gmail.com>
2020-09-14 17:11:55 +0200

    chore(deps): update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,16 +25,12 @@ module.exports = {
     'react/destructuring-assignment': 'off',
     // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
     'react/jsx-filename-extension': 'off',
-    // Use function hoisting to improve code readability
-    'no-use-before-define': [
-      'error',
-      { functions: false, classes: true, variables: true },
-    ],
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
     '@typescript-eslint/explicit-function-return-type': [
       'error',
       { allowExpressions: true, allowTypedFunctionExpressions: true },
     ],
+    'no-use-before-define': [0],
     '@typescript-eslint/no-use-before-define': [
       'error',
       { functions: false, classes: true, variables: true, typedefs: true },
